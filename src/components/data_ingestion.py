0e8602b45e28c2047e08cc9c5ddbe08a1f671316
nandhkumar77 <nandhkumargs8877@gmail.com>
2025-07-21 03:30:01 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,98 @@
+import os 
+import sys
+from src.execption import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass# used to create classes that are primarily used to store data with little to no logic
+
+"""
+ What is Boilerplate Code?
+Boilerplate code refers to standard or repetitive code that you need to write in many places with little or no variation — just to make something work, even though it doesn't do anything unique.
+
+
+os, sys: For system-level operations (like path handling and error reporting).
+
+pandas: For reading and manipulating datasets.
+
+train_test_split: For splitting your dataset into training and testing sets.
+
+@dataclass: Allows easy creation of data-holding classes without boilerplate code.
+
+from src.exception import CustomException: Custom exception class for better error tracking (make sure the CustomException class is implemented properly).
+
+from src.logger import logging: Logging module (you’ve already built this in your previous file).
+
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+    name: str
+    age: int
+
+"""
+
+
+''''
+| Function           | Purpose                   |
+| ------------------ | ------------------------- |
+| `os.getcwd()`      | Get current directory     |
+| `os.makedirs()`    | Create directories        |
+| `os.listdir()`     | List contents of a folder |
+| `os.path.exists()` | Check if path exists      |
+| `os.path.join()`   | Build safe path strings   |
+| `os.remove()`      | Delete a file             |
+| `os.rmdir()`       | Delete an empty folder    |
+| `os.rename()`      | Rename file/folder        |
+'''
+
+@dataclass
+class DataIngestionConfig:
+    """1.The @dataclass only defines paths — it doesn’t create anything
+     2. data class just hold values of the variables """
+    train_data_path=os.path.join("artifacts",'train_data.csv')## It doesn’t create any folders. It’s just telling Python, “Here’s where I want to save my training data later.”
+    test_data_path=os.path.join("artifacts",'test_data.csv')
+    raw_data_path=os.path.join("artifacts",'raw_data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info("read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)#	Actually creates the directory where the file will be saved
+
+            '''
+            Let’s say this is the value of self.ingestion_config.train_data_path:'artifacts/train_data.csv'
+
+            but  os.path.dirname('artifacts/train_data.csv') 
+            # returns: 'artifacts' -directory 
+
+            '''
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("ingestion of the data is completed")
+            return (
+                self.ingestion_config.train_data_path,self.ingestion_config.test_data_path,
+            )
+        except Exception as e :
+            raise CustomException(e, sys)
+        
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
